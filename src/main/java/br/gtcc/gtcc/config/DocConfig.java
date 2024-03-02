984c0369c6a948731631d44d7930229e0c1d1d3a
SammLopes <sam.v.p.lopes@gmail.com>
2024-03-02 12:33:17 -0300

    Update DocCOnfig, add configuration of swaager document

--- src/main/java/br/gtcc/gtcc/config/DocConfig.java
+++ src/main/java/br/gtcc/gtcc/config/DocConfig.java
@@ -4,10 +4,29 @@
  */
 package br.gtcc.gtcc.config;
 
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
+
 /**
  *
  * @author mrbee
+ * 
+ * Classe de configuração do Swaager para documentação da API, não é para TESTE !
  */
+@Order(2)
 public class DocConfig {
-    
+    @Bean
+    public OpenAPI customOpenAPI(){
+        return new OpenAPI().info(new Info()
+                .title("API em NoSQL Nitrite GTCC")
+                .version("0.0.1")
+                .contact(new Contact().email("vix.bay.group@gmail.com")
+                .name("Arthur, Alessandro Santos, Hugo Alves, Douglas Lessa, Pedro Pompermayer, Samuel Paviotti"))
+                .description("<h1>API GTCC</h1><p>Repositório ainda não esta disponível</p>"
+                ));
+    }
 }
