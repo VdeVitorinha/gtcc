88b0a74926a0c6ea200e935734bc9f1f96f81ced
SammLopes <sam.v.p.lopes@gmail.com>
2024-03-03 01:06:58 -0300

    Update methods getUsers() ,getAllUsers(), and updateUsers()

--- src/main/java/br/gtcc/gtcc/services/UserServices.java
+++ src/main/java/br/gtcc/gtcc/services/UserServices.java
@@ -6,7 +6,10 @@ package br.gtcc.gtcc.services;
 
 import br.gtcc.gtcc.model.Users;
 import java.util.List;
+import org.dizitart.no2.collection.FindOptions;
+import org.dizitart.no2.common.SortOrder;
 import org.dizitart.no2.common.WriteResult;
+import org.dizitart.no2.filters.FluentFilter;
 import static org.dizitart.no2.filters.FluentFilter.where;
 import org.dizitart.no2.repository.ObjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -84,15 +87,48 @@ public class UserServices {
     }
     
     public Users updateUsers(Users users){
+           Users getU = this.getUsers(users);
+        
+        if( users != null ){
+            
+            if( users.getRegistration() != null ){
+            
+                if( getU != null ){
+                
+                    WriteResult result = repositoryUsers.update(where("id").eq(users.getRegistration()), users);
+                    users = (Users) result;
+            
+                } else {
+
+                    return null;
+
+                }
+                
+            } else {
+                
+                return null;
+                
+            }            
+            
+        } else {
+            
+            return null;
+            
+        }
+        
         return users;
     }
 
     public List<Users> getAllUsers(){
-        return null;
+
+        return repositoryUsers.find(FindOptions.orderBy("name", SortOrder.Ascending)).toList();
+
     }
     
     public Users getUsers(Users users){
-        return null;
+        
+        return repositoryUsers.find(FluentFilter.where("registration").eq(users.getRegistration())).firstOrNull();
+    
     }
     
 }
